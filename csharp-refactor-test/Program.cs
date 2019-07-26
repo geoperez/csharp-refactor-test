a99a1ad74a3a2e0b538ab911278c1535e8fb91fd
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-07-26 13:52:21 -0500

    Update

---
+++ csharp-refactor-test/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace csharp_refactor_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guess the magic word");
+
+            var data = GetData().GetAwaiter().GetResult();
+            var output = ProcessData(data);
+            var results = new List<bool>();
+
+            foreach (var n in output)
+            {
+                Console.WriteLine("Guess the letter:");
+
+                var letter = Console.ReadKey().Key;
+
+                if (letter.ToString()[0] == n)
+                    results.Add(true);
+                else
+                    results.Add(false);
+
+                Console.WriteLine();
+            }
+
+            var isValid = true;
+
+            foreach (var r in results)
+            {
+                if (r == true && isValid == true)
+                    isValid = true;
+                else if (r == true && isValid == false)
+                    isValid = false;
+                else if (r == false && isValid == true)
+                    isValid = false;
+                else if (r == false && isValid == false)
+                    isValid = false;
+            }
+
+            PrintResult(isValid);
+
+            Console.WriteLine("Continue? (y/n)");
+
+            if (Continue())
+                StartGame();
+        }
+
+        private static async Task<string> GetData()
+        {
+            return "HOLA";
+        }
+
+        private static char[] ProcessData(string data) => data.ToCharArray();
+
+        private static void PrintResult(bool isValid)
+        {
+            if (isValid)
+                Console.WriteLine("WINNER");
+            else
+                Console.WriteLine("LOSER");
+        }
+
+        private static bool Continue() => Console.ReadKey().Key == ConsoleKey.Y ? true : false;
+    }
+}
